972c87eab935b4b86543bc528e23df4d25211242
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-03-23 16:48:35 -0400

    Add finishing touches regarding UI.

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -10,18 +10,29 @@ function computerChoice(){
     };
     return randomChoice
 }
+
+// function winCondition(){
+//     if(playerScore === 5){
+//         winner = true
+//         message.textContent = 'Player Wins!!'
+//     } else if (comScore === 5){
+//         winner = true
+//         message.textContent = 'Computer Wins!!'
+//         return 
+//     }
+    
+// }
 playerScore = 0;
 comScore = 0;
 function playRound(args){
     comChoice = computerChoice()
     let playerChoice = args
-    
         if(playerChoice == comChoice){
             console.log()
             let tie = true;
             message.textContent = `It's a tie! you both picked ${comChoice}. Player score: ${playerScore}. Computer Score: ${comScore}`
         // Player picks ROCK
-        } else if(playerChoice == rock.textContent){
+        } else if(playerChoice == 'rock'){
             if(comChoice == 'scissors'){
                 message.textContent = ''
                 playerScore += 1;
@@ -33,7 +44,7 @@ function playRound(args){
             }
             
         // Player picks PAPER
-        } else if(playerChoice == paper.textContent){
+        } else if(playerChoice == 'paper'){
             if(comChoice == 'scissors'){
                 comScore += 1;
                 message.textContent = `You lost, scissors beats paper. Player score is: ${playerScore}. Computer Score is: ${comScore}`
@@ -43,7 +54,7 @@ function playRound(args){
             } 
             
         //Player picks SCISSORS
-        } else if(playerChoice == scissors.textContent){
+        } else if(playerChoice == 'scissors'){
             if(comChoice == 'rock'){
                 console.log('')
                 comScore += 1;
@@ -54,62 +65,65 @@ function playRound(args){
                 message.textContent = `You win! Scissors beats paper. Player score is: ${playerScore}. Computer Score is: ${comScore}`
             }
             
+            
+            
+            
         }
-        for(let i =0; i<5; i++){
-            playRound()
-            if(tie= true){
-                i--;
-            }
-            if(comScore == 5){
-                console.log("The computer wins! better luck next time")
-                break
-            } else if (playerScore == 5){
-                console.log('You win! You beat the computer')
-                break
-            }
-
+        
+        let tryAgain =  document.querySelector('#tryAgain')
+        // Stops you from being able to click on the choices, and brings up a try again button to refresh
+        if(comScore == 5){
+            message.textContent = "The computer wins! better luck next time"
+            document.getElementById('rock').disabled = true
+            document.getElementById('paper').disabled = true
+            document.getElementById('scissors').disabled = true
+            tryAgain.style = 'visibility: visible;'
+        } else if (playerScore == 5){
+            message.textContent = 'You win! You beat the computer'
+            document.getElementById('rock').disabled = true
+            document.getElementById('paper').disabled = true
+            document.getElementById('scissors').disabled = true
+            tryAgain.style = 'visibility: visible;'
         }
+        
     }
 
 
-    function playGame(comScore, playerScore){
-        // for(let i =0; i<5; i++){
-        //     playRound()
-        //     if(tie= true){
-        //         i--;
-        //     }
-        //     if(comScore == 5){
-        //         console.log("The computer wins! better luck next time")
-        //         break
-        //     } else if (playerScore == 5){
-        //         console.log('You win! You beat the computer')
-        //         break
-        //     }
-
-        // }
-    }
-
-let rock = document.querySelector('#rock')
-rock.textContent = 'rock'
-let paper = document.querySelector('#paper')
-paper.textContent = 'paper'
-let scissors = document.querySelector('#scissors')
-scissors.textContent = 'scissors'
+    // function playGame(comScore, playerScore){
+    //     for(let i =0; i<5; i++){
+    //         playRound();
+    //         if(tie= true){
+    //             i--;
+    //         }
+    //         if(comScore == 5){
+    //             console.log("The computer wins! better luck next time")
+    //             break
+    //         } else if (playerScore == 5){
+    //             console.log('You win! You beat the computer')
+    //             break
+    //         }
+    // }
+    // }
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
 let message = document.querySelector('#message')
 message.setAttribute('style', 'font-size:20px;')
 
 // buttons.addEventListener('click', playGame())
-const player = [rock, paper, scissors];
+// const player = [rock, paper, scissors];
 
 rock.addEventListener('click', function(){
-    play = player[0].textContent;
+    play = 'rock'
     playRound(play);
+
+    
 });
 paper.addEventListener('click', function(){
-    play = player[1].textContent;
+    play = 'paper';
     playRound(play);
 });
 scissors.addEventListener('click', function(){
-    play = player[2].textContent;
+    play = 'scissors';
     playRound(play);
 });
